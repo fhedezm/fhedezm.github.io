03e06e57e90e410470c78ecfd34924cf69d1c4c3
fhedezm <130528530+fhedezm@users.noreply.github.com>
2023-05-04 20:00:23 -0600

    Update embed_sopotify.js

--- javascript/embed_sopotify.js
+++ javascript/embed_sopotify.js
@@ -1,11 +1,11 @@
-$(window).on("load resize", function () {
+$(window).on("load_resize", function () {
     $('iframe[src*="embed.spotify.com"]').each(function () {
       $(this).css("width", $(this).parent().css("width"));
       $(this).attr("src", $(this).attr("src"));
       $(this).removeClass("loaded");
-  
+	  debugger;
       $(this).on("load", function () {
         $(this).addClass("loaded");
       });
     });
-});
+}); 
